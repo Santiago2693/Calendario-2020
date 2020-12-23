fa6ca124f786afef10d93716e50aa209ddc67cd4
Mikkel-14 <jose.munoz01@epn.edu.ec>
2020-12-22 22:42:24 -0500

    Comprobaciones adicionales, no parece haber error

--- src/ejercicio4.py
+++ src/ejercicio4.py
@@ -1,51 +1,22 @@
-PATH='puzzle_input/ejercicio4.txt'
-"""
-path='puzzle_input/ejercicio4.txt'
-total=0
-camposValidos=0
-camposObligatorios = 'byr: iyr: eyr: hgt: hcl: ecl: pid:'
-
-with open(path) as p:
-            for line in p:
-                if not line=="\n" :
-                        for x in camposObligatorios.split():
-                            if not line.find(x)==-1:
-                                camposValidos=camposValidos+1
-                                if camposValidos>=7:
-                                    total+=1
-                else:
-                    camposValidos=0
-
-print (total)
-
-
-
-listaDiccionarios=list()
-with open(PATH) as f:
-    for line in f:
-
-        for x in line.split(':'):
-
-
-
-
-
-"""
 import re
-def main():
+import json
+PATH='puzzle_input/ejercicio4.txt'
+PATH2= 'puzzle_input/ejercicio4M.txt'
+VALIDOS= 'puzzle_input/validos.txt'
+def main(ruta):
     pasaportes = list()
     camposObligatorios = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
-
-    arch = open(PATH)
+    arch = open(ruta)
     tamano = len(arch.readlines())-1
     arch.close()
+    bandera = 0
     listaDiccionarios = list()
-    with open(PATH) as f:
+    with open(ruta) as f:
         string = ""
         temp = list()
         for line in f:
             string += " "+line.strip()
-            if line == "\n" :
+            if line == "\n" or bandera == tamano:
                 temp = string.strip().split(" ")
                 temp2 = list()
                 tabla = dict()
@@ -55,35 +26,41 @@ def main():
                 if (set(camposObligatorios).issubset(set(tabla.keys()))) and validarPasaporte(tabla):
                     listaDiccionarios.append(tabla)
                 string =""
+            bandera+=1
 
-    #print(listaDiccionarios)
+    archive = json.dumps(listaDiccionarios, indent=4)
+    nuevo = open("puzzle_input/resultado.json","w")
+    nuevo.write(archive)
+    nuevo.close()
     print("La lista de pasaportes validos es:",len(listaDiccionarios))
+    return listaDiccionarios
 
 
 def validarPasaporte(pasaporte):
     eyesList=['amb', 'blu', 'brn', 'gry', 'grn','hzl', 'oth']
     value = int(pasaporte["byr"]) in range(1920,2003)
-    value = value and int(pasaporte["iyr"]) in range(2010,2021)
-    value = value and int(pasaporte["eyr"]) in range(2020, 2031)
-    """
-    value = value and int(pasaporte["iyr"]) in range(2010,2021)
-    value = value and  pasaporte["ecl"] in eyesList
+    value = value and (int(pasaporte["iyr"]) in range(2010,2021))
+    value = value and (int(pasaporte["eyr"]) in range(2020, 2031))
+    value = value and (int(pasaporte["iyr"]) in range(2010,2021))
+    value = value and  (pasaporte["ecl"] in eyesList)
     altura = pasaporte["hgt"]
     if (altura[len(altura)-2:len(altura)]=='cm'):
-        value = value and int(pasaporte["hgt"][:-2]) in range(150,194)
+        value = value and (int(altura[:-2]) in range(150,194))
+    elif (altura[len(altura)-2:len(altura)]=='in'):
+        value =  value and (int(altura[:-2]) in range(59, 77))
     else:
-        value =  value and int(pasaporte["hgt"][:-2]) in range(59, 77)
-    patronCabello = re.compile("#[0-9a-f]{6}")
+        value = False
+    patronCabello = re.compile("^#[0-9a-f]{6}")
     if patronCabello.match(pasaporte["hcl"]) == None:
-        value = value and False
+        value = False
     else:
         value = value and True
-    patron = re.compile("[0-9]{9}")
+    patron = re.compile("^[0-9]{9}")
     if patron.match(pasaporte["pid"]) == None:
-        value = value and False
+        value = False
     else:
         value = value and True
-    """
+
     return value
 
-main()
+main(PATH)
