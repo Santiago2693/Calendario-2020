48d9080162429dcde577de93e34675199fdf380c
Santiago2693 <69229850+Santiago2693@users.noreply.github.com>
2020-12-22 19:05:48 -0500

    help XD

---
+++ src/ejercicio4.py
@@ -0,0 +1,24 @@
+path='puzzle_input/ejercicio4.txt'
+total=0
+camposValidos=0
+camposObligatorios = 'byr: iyr: eyr: hgt: hcl: ecl: pid:'
+
+with open(path) as p:
+            for line in p:
+                if not line=="\n":
+                        print (line)
+                        for x in camposObligatorios.split():
+
+                            if not line.find(x)==-1:
+
+                                camposValidos=camposValidos+1
+                                print(camposValidos)
+
+                else:
+
+                    if camposValidos>=7:
+                        total=total+1
+                    print(total)
+                    camposValidos=0
+
+print (total)
