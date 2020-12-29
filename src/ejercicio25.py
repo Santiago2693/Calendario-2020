c0bc7080c80b4365352a04b4720a5c394aa09b09
Mikkel-14 <jose.munoz01@epn.edu.ec>
2020-12-29 15:58:41 -0500

    Parte 1 del ejercicio 25 resuelto

---
+++ src/ejercicio25.py
@@ -0,0 +1,26 @@
+pkCARD = 1327981
+pkDOOR = 2822615
+sNumber = 7 # no se si este valor cambia por cada uno
+div = 20201227
+
+def loopSize(clave, valorInicial =1):
+    n = 0
+    while not valorInicial == clave:
+        valorInicial *= sNumber
+        valorInicial %= div
+        n+=1
+    return n
+
+def claveEncripcion(subjectNumber, loopSize):
+    valorInicial = 1
+    for i in range(loopSize):
+        valorInicial *= subjectNumber
+        valorInicial %= div
+    return valorInicial
+
+def main():
+    lsCard = loopSize(pkCARD)
+    key = claveEncripcion(pkDOOR,lsCard)
+    print("La clave de encripcion es: ",key)
+
+main()
