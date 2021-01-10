a74f5df6a8df6817ed1debb57096f11f899d0b0e
Santiago2693 <69229850+Santiago2693@users.noreply.github.com>
2021-01-10 12:04:10 -0500

    Logica de la parte 2 del ejercicio 19 iniciada y eliminacion del json

--- src/ejercicio19S.py
+++ src/ejercicio19S.py
@@ -50,14 +50,42 @@ def recursivo(inicioCadena):
                     for b in listaAuxiliar:
                         unirReglas.append(b+a)
                 listaAuxiliar=unirReglas.copy()
+
         listaDePosibilidades+=listaAuxiliar
     reglasConsideradas[inicioCadena]=listaDePosibilidades
 
     return listaDePosibilidades
 
 gramaticaGenerada=recursivo(0)
+
 contador=0
 for a in cadenas:
     if a in gramaticaGenerada:
         contador+=1
-print(contador)
+print("El conjunto de las cadenas validas para la parte 1 es de:",contador)
+#para la parte 2 la regla 8 antes era 42 ahora es 42 |42 8
+#y la regla 11 era 42 31 ahora es 42 31| 42 11 31, por lo cual nos enfrentamos
+#a que se puede entrar en un bucle infinito al genera las cadena 42 42 42, ademas
+#la regla 11 ahora tiene 3 posibilidades deferentes en lugar de 2 como las otras
+def recursivo2():
+    #si analizamos la gramatica si aparece los numeros que se repetiran si se genera
+    #un bucle infinito son 42 y 31, por lo cual de las reglas consideradas se va a sacar su
+    #transformacion para estas reglas
+    regla42=reglasConsideradas[42]
+    regla31=reglasConsideradas[31]
+    #for r in regla42:
+        #print(r)
+    #for r in regla31:
+        #print(r)
+    #afortunadamente ambas reglas generan gramaticas
+    #de la misma longitud, por lo cual podemos obtener un
+    #punto de quiebre
+    puntoQuiebre=len(regla31[0])
+    #para este punto hallar todas las reglas posibles no es eficiente
+    #por lo cual ahora se va a analizar la cadena y encontraar si se
+    #deriva de 0
+    for cadena in cadenas:
+        quiebre42 = [False for a in range(len(cadena)//puntoQuiebre)]
+        quiebre31 = [False for b in range(len(cadena)//puntoQuiebre)]
+
+recursivo2()
